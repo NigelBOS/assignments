cb31980e0826176b45e97bd1a524a77ea94df397
NigelBOS <166861205+NigelBOS@users.noreply.github.com>
2026-07-08 02:46:33 -0700

    Update Arith4.asm

--- CISC211/W3/Arith4.asm
+++ CISC211/W3/Arith4.asm
@@ -1,9 +1,9 @@
 section .text
-        global _start
+    global _start
 
 _start:
-        ;put code here:
-      	mov ebx, [var2] ;denominator
+    ;put code here:
+      mov ebx, [var2] ;denominator
 	sub ebx, 3
 
 	mov eax, [var1] ; numerator
@@ -11,19 +11,19 @@ _start:
 	
 	cdq ;sing-extends eax into EDX:EAX
 	idiv ebx ;divides edx:eax by ebx
-        mov [result], eax ;store in result
+    mov [result], eax ;store in result
 
-        ;exit stuff
-        mov eax, 1
-        mov ebx, 0
-        int 0x80
+    ;exit stuff
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
 
 section .data
-        var1 dd 4
-        var2 dd 5
-        var3 dd 6
-        var4 dd 7
+    var1 dd 4
+    var2 dd 5
+    var3 dd 6
+    var4 dd 7
 
 section .bss
-        ;the uninitialized variable result
-        result resd 1
+    ;the uninitialized variable result
+    result resd 1
