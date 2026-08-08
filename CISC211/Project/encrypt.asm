a64598536e3b3ea8bfd7ec369d3a2d40ba88c11c
NigelBOS <166861205+NigelBOS@users.noreply.github.com>
2026-08-08 01:48:10 -0700

    Update encrypt.asm

--- CISC211/Project/encrypt.asm
+++ CISC211/Project/encrypt.asm
@@ -34,6 +34,10 @@ _start:
     dec eax		;subtracts 1 to ignore \n
     mov [key2_len], eax	;saves the true length of the key into key_len
 
+    ;prevent division by 0, edge case
+    cmp dword [key2_len], 0
+    je exit
+
     ;The Encryption loop
     mov dword [idx], 0	;reset index to 0
 encrypt_loop:
@@ -47,12 +51,30 @@ encrypt_loop:
     mov ecx, [key2_len]	;ecx = key length
     div ecx		;division operation, eax/ecx remainder goes to edx
 
-    ;prepare char bytes and perform xor
+    ;prepare char bytes and perform raw xor
     mov eax, [idx]		;restore the idx offest, goes to correct char
     mov bl, [msg_buf + eax]	;the plain text byte
     mov cl, [key_buf + edx]	;the key byte from the mod operation 
-    xor bl, cl			;xor
-    mov [enc_msg + eax], bl	;save the encrypted msg
+    xor bl, cl			;raw xor
+    mov [enc_msg1 + eax], bl	;save the encrypted msg
+
+    ;make printable ASCII range [32,126]
+printable:
+    cmp bl, 32    ;compares bl to 32
+    jb add_32     ;if bl < 32, jump, add 32, unsigned jump
+
+    cmp bl, 126    ;compares bl to 126
+    ja sub_32      ;if bl > 126, jump, sub 32, unsigned jump
+    jmp store_printable
+add_32:
+    add bl, 32     ;after adding, should be in print range
+    jmp printable  ;go back and check again
+sub_32:
+    sub bl, 32     ;after subtracting should be in print range
+    jmp printable  ;go back and check again
+
+store_printable:
+    mov [enc_msg2 + eax], bl  ;save printable byte
 
     inc dword [idx]		;increment the index for the loop
     jmp encrypt_loop		;repeat loop
@@ -74,9 +96,9 @@ decrypt_loop:
 
     ;prepares char bytes and performs xor
     mov eax, [idx]		;eax to current index
-    mov bl, [enc_msg + eax]	;encrypted msg bytes
+    mov bl, [enc_msg1 + eax]	;encrypted msg bytes
     mov cl, [key_buf + edx]	;key bytes from prior mod operation
-    xor bl, cl			;xor
+    xor bl, cl			;xor, undoing original xor
     mov [dec_msg + eax], bl	;save the decrypted msg 
 
     inc dword [idx]		;increment the index for the loop
@@ -93,19 +115,22 @@ write_output_file:
     ;Write results to the file
 write_file:
     ;Write "Plain text: " + input word \n	
-    ; To output.txt
+    ;To output.txt
+    ;"Plain text: "
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, plain1
     mov edx, plain1_len
     int 0x80
 
+    ;input word
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, msg_buf
     mov edx, [msg_len]
     int 0x80
 
+    ;new line
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, newline
@@ -113,6 +138,7 @@ write_file:
     int 0x80
 
     ;Write "Key: " + input key word \n
+    ;"Key: "
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, key1
@@ -126,6 +152,7 @@ write_file:
     mov edx, [key2_len]
     int 0x80
 
+    ;new line
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, newline
@@ -133,18 +160,21 @@ write_file:
     int 0x80
 
     ;Write "Encrypted text: " + encryption result \n
+    ;"Encrypted text: "
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, encry
     mov edx, encry_len
     int 0x80
 
+    ;encryption result
     mov eax, 4
     mov ebx, [fd_out]
-    mov ecx, enc_msg
+    mov ecx, enc_msg2
     mov edx, [msg_len]
     int 0x80
 
+    ;new line
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, newline
@@ -160,7 +190,7 @@ write_file:
 
     mov eax, 4
     mov ebx, 1          ;stdout
-    mov ecx, enc_msg
+    mov ecx, enc_msg2
     mov edx, [msg_len]
     int 0x80
 
@@ -171,18 +201,21 @@ write_file:
     int 0x80
 
     ;Write "Decrypted text: " + decryption result \n
+    ;"Decrypted text: "
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, decry
     mov edx, decry_len
     int 0x80
 
+    ;decryption result
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, dec_msg
     mov edx, [msg_len]
     int 0x80
 
+    ;new line
     mov eax, 4
     mov ebx, [fd_out]
     mov ecx, newline
@@ -239,11 +272,11 @@ section .data
 section .bss
     msg_buf     resb 128
     key_buf     resb 128
-    enc_msg     resb 128
+    enc_msg1    resb 128    ;raw xor message
+    enc_msg2    resb 128    ;Printable encrypted msg
     dec_msg     resb 128
     
     msg_len     resd 1
     key2_len     resd 1
     idx         resd 1    ; index for loop
     fd_out      resd 1
-
